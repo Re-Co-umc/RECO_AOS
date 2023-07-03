4d89d80a40cb5cc19abd9292deafb37c21ab2017
jung0115 <jungim7490@gmail.com>
2023-07-03 19:12:03 +0900

    feat #1: 하단바 생성(디자인 미적용)

---
+++ app/src/main/java/com/umc/reco_aos/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.umc.reco_aos.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umc.reco_aos.R
+
+
+// 홈: 지도
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
