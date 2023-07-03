9854d67e046223313afe171e2ec41a0e1049545b
jung0115 <jungim7490@gmail.com>
2023-07-03 20:17:46 +0900

    feat #1: T-map API 연결 > 지도

--- app/src/main/java/com/umc/reco_aos/home/HomeFragment.kt
+++ app/src/main/java/com/umc/reco_aos/home/HomeFragment.kt
@@ -5,17 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.skt.Tmap.TMapView
 import com.umc.reco_aos.R
+import com.umc.reco_aos.databinding.FragmentHomeBinding
 
 
 // 홈: 지도
 class HomeFragment : Fragment() {
+    // ViewBinding Setting
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    // T-map Service Key
+    private val TMAP_SERVICE_KEY = "TvgSIt2YIV40wmWE6sgoz190w6oRRFK064xqIYKI"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 지도: T-map
+        val tmapview = TMapView(context)
+        tmapview.setSKTMapApiKey(TMAP_SERVICE_KEY)
+        binding.linearMap.addView(tmapview)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
